f4e5ba19a32d0bb132f89e9f8cd6bcb7111fbf3e
mouni <mounika.g1162@gmail.com>
2022-11-22 10:12:57 +0530

    first commit

---
+++ country/script.js
@@ -0,0 +1,56 @@
+let  row=document.querySelector(".row")
+function creat(element){
+let data=document.createElement("element")
+return data
+}
+
+let url="https://restcountries.com/v3.1/all/"
+fetch(url).then((res)=>res.json()).then((data)=>{
+data.forEach((ele)=>
+{
+let div=creat("div")
+div.className="col-sm-6 col-md-4 col-lg-4 col-xl-4"
+let filename=temp(ele)
+div.innerHTML=filename
+row.appendChild(div)
+})
+})
+let temp=(country)=>{
+    let tem=`<div class="card h-100">
+    <div class="card-header">${country.name.common}</div>
+    <div class="card-body">
+    <img class="flag" src="${country.flags.png}" width="300px" height="300px"
+    <div class=align>
+    <p class="text"> <b> Capital:</b>${country.capital}</p>
+    <p class="text"> <b> Region:</b>${country.region}</p>
+    <p class="text"> <b> Population:</b>${country.population}</p>
+    <br></div>
+    <button class="btn btn-primary" onclick="weather([${country.latlng[0]},${country.latlng[1]}],'${country.name.common}')">weather</button>
+    <p id="load${country.name.common}"></p>
+    </div>
+    </div><br>
+    `;
+    return tem
+}
+let weather=(lan,name)=>{
+    let [lat,lon]=lan
+    let l=document.getElementById(`load${name}`)
+    l.innerHTML="loading..."
+    let url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9bf59c67c0d408cc5e1c4877f3e4d9d5`
+    fetch(url).then((res)=>res.json()).then((data)=>{
+        
+        
+        //weather msg template
+        let weatherMsg=`
+        Countrty : ${data.name}
+        latiture :  ${lat}   longiture :${lon}
+        Weather  :  ${data.weather[0].description}
+        Wind speed : ${data.wind.speed}
+        temperature : ${data.main.temp} 
+        `;
+        
+        //we are going to show weather report via alert.
+        alert(weatherMsg);
+        l.innerHTML="";
+})
+}
